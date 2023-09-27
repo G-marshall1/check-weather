f2e9a7922dcff22422bb4ef31223d1781f1ef71c
Graham Marshall <gmarshalltech1@gmail.com>
2023-09-26 22:16:45 -0600

    Added js comments

---
+++ script.js
@@ -0,0 +1,12 @@
+//connect weather api to allow search ability on click
+
+//When putting a city in the search bar I can click search to pull its local weather
+
+// After I click search, A history is created un the search bar
+
+//when a city is searched, the current local weather appears with the 
+//city name, date, and icon,  with temp, wind, and humidity under it.
+
+// The 5 day forecast also appears under that in 5 idividual cards displaying a different day
+
+// each card will also contain the date, icon, temp, wind, humidity
